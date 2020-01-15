5e3492f90cb0023532a8957112b10434cbf1b247
unknown <juan.balsa.ds@gmail.com>
2020-01-16 00:02:33 +0100

    hola

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!bin/bash
-echo "Starting downloading" > /home/jbalsads/results/test.txt;
-date > /home/jbalsads/results/test.txt;
+echo "Starting downloading" >> /home/jbalsads/results/test.txt;
+date >> /home/jbalsads/results/test.txt;
 for i in {00..50}; do sleep 1; echo $i; done
-echo "Finish" > /home/jbalsads/results/test.txt;
-date > /home/jbalsads/results/test.txt;
+echo "Finish" >> /home/jbalsads/results/test.txt;
+date > >/home/jbalsads/results/test.txt;
 sudo poweroff
 exit 0;
 
