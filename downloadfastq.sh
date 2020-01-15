9235bf19411d3e939dcf42f029296a59d92f9431
unknown <juan.balsa.ds@gmail.com>
2020-01-15 23:51:07 +0100

    hola

--- downloadfastq.sh
+++ downloadfastq.sh
@@ -1,5 +1,6 @@
 #!bin/bash
-echo "Starting downloading";
+echo "Starting downloading" > /home/jbalsads/results/downloadfastq.txt;
+date > /home/jbalsads/results/downloadfastq.txt;
 #Download ALL the files from ftp server
 wget -r ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/MTAB/E-MTAB-6967/ -P /home/jbalsads/yard/fastq_path
 #download the reference dataset and uncompressed it
@@ -8,3 +9,6 @@ wget http://cf.10xgenomics.com/supp/cell-exp/refdata-cellranger-mm10-3.0.0.tar.g
 cd /home/jbalsads/yard/ref_genome
 tar -zxvf refdata-cellranger-mm10-3.0.0.tar.gz
 rm -r refdata-cellranger-mm10-3.0.0.tar.gz
+echo "Succesfully finish" > /home/jbalsads/results/downloadfastq.txt;
+date > /home/jbalsads/results/downloadfastq.txt;
+sudo poweroff;
--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!bin/bash
-echo "Hola mundo!";
-mkdir app;
-echo "Fin";
+echo "Starting downloading" > /home/jbalsads/results/test.txt;
+date > /home/jbalsads/results/test.txt;
 for i in {00..50}; do sleep 1; echo $i; done
+echo "Finish" > /home/jbalsads/results/test.txt;
+date > /home/jbalsads/results/test.txt;
 sudo poweroff
 exit 0;
 
