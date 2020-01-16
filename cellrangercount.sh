948f3bc0c2ee1ae60da8b5b92c3ba27a5a19b021
unknown <juan.balsa.ds@gmail.com>
2020-01-16 16:39:33 +0100

    last

--- cellrangercount.sh
+++ cellrangercount.sh
@@ -1,11 +1,13 @@
 #!bin/bash
-echo "Starting downloading";
+echo "Starting Count" >> /home/jbalsads/results/count.txt;
+date >> /home/jbalsads/results/count.txt;
 cd /home/jbalsads/yard/fastq_path/ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/MTAB/E-MTAB-6967
 find -name "*.fastq.gz" | cut -c3-18 | uniq -d | grep -v / | tr '\n' ','> list.txt
 read var < list.txt;
 cd /home/jbalsads/results
 cellranger count --id=sample --transcriptome=/home/jbalsads/yard/ref_genome/refdata-cellranger-mm10-3.0.0 --fastqs=/home/jbalsads/yard/fastq_path/ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/MTAB/E-MTAB-6967 --sample=$var
-#for i in {00..50}; do sleep 1; echo $i; done
+echo "Starting Count" >> /home/jbalsads/results/count.txt;
+date >> /home/jbalsads/results/count.txt;
 sudo poweroff
 
 
--- downloadfastq.sh
+++ downloadfastq.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-echo "Starting downloading" > /home/jbalsads/results/downloadfastq.txt;
-date > /home/jbalsads/results/downloadfastq.txt;
+echo "Starting downloading" >> /home/jbalsads/results/downloadfastq.txt;
+date >> /home/jbalsads/results/downloadfastq_resume.txt;
 #Download ALL the files from ftp server
 wget -r ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/MTAB/E-MTAB-6967/ -P /home/jbalsads/yard/fastq_path
 #download the reference dataset and uncompressed it
@@ -9,6 +9,6 @@ wget http://cf.10xgenomics.com/supp/cell-exp/refdata-cellranger-mm10-3.0.0.tar.g
 cd /home/jbalsads/yard/ref_genome
 tar -zxvf refdata-cellranger-mm10-3.0.0.tar.gz
 rm -r refdata-cellranger-mm10-3.0.0.tar.gz
-echo "Succesfully finish" > /home/jbalsads/results/downloadfastq.txt;
-date > /home/jbalsads/results/downloadfastq.txt;
+echo "Succesfully finish" >> /home/jbalsads/results/downloadfastq.txt;
+date >> /home/jbalsads/results/downloadfastq_resume.txt;
 sudo poweroff;
