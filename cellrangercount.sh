5d8ab92f97d8e405619d480c381db1f685103be6
unknown <juan.balsa.ds@gmail.com>
2020-01-22 09:47:26 +0100

    Hola

--- cellrangercount.sh
+++ cellrangercount.sh
@@ -5,7 +5,7 @@ do
 	echo "Starting Count" >> /home/jbalsads/results/count_.txt;
 	date >> /home/jbalsads/results/count_.txt;
 	cd /home/jbalsads/yard/fastq_path/ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/MTAB/E-MTAB-6967
-	find -name "*.fastq.gz" | cut -c3-18 | uniq -d |head -n 10000| grep -v / | tr '\n' ','> list.txt
+	find -name "*.fastq.gz" | cut -c3-18 | uniq -d |head -n 5000| grep -v / | tr '\n' ','> list.txt
 	read var < list.txt;
 	cd /home/jbalsads/results
 	cellranger count --id=sample --transcriptome=/home/jbalsads/yard/ref_genome/refdata-cellranger-mm10-3.0.0 --fastqs=/home/jbalsads/yard/fastq_path/ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/MTAB/E-MTAB-6967 --sample=$var
